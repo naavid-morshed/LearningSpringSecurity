aa3055f7cbf1a452a415b05a012034c3dba6f7d3
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-03-17 12:27:36 +0600

    no msg

---
+++ src/main/java/com/example/learningbasicspringsecurity/LearningBasicSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningbasicspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBasicSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningBasicSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learningbasicspringsecurity/LearningBasicSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningbasicspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBasicSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
